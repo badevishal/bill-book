8491352e29b127f843554f198d0244db4e799d9c
Vishal-Bade <vishalbade1997@gmail.com>
2024-06-13 12:38:33 +0530

    Add SUN logo to theme button

--- src/components/ThemeBtn.jsx
+++ src/components/ThemeBtn.jsx
@@ -27,7 +27,7 @@ function ThemeBtn() {
           onChange={themeBtnEvent}
           className="sr-only peer"
         />
-        <p className="w-11 h-6 text-4xl" title="Theme">{themeLogo ? "🌙" : "⚙️"}</p>
+        <p className="w-11 h-6 text-4xl" title="Theme">{themeLogo ? "🌙" : "☀️"}</p>
         {/* <div className="w-11 h-6 bg-gray-200 rounded-full after:h-5 after:w-5 after:rounded-full after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border after:border-gray-300 after:transition-all peer peer-checked:after:translate-x-full peer-checked:bg-blue-600"></div> */}
       </label>
     </div>
